081babf6306d82bf71036c0b0bf1c544768a6789
James Thomson <jthomson@pivotal.io>
2016-05-06 15:13:37 -0400

    TURNIN ON THE HOSE
    Signed-off-by: Gavin Enns <genns@pivotal.io>

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   get 'env/:name', to: 'st_hasselhoff_community_pool#show'
+  get 'git', to: 'st_hasselhoff_community_pool#git'
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
